b6dee80e76ec8eec22094a81a2bc7b9392685998
Seunggi Lee <seunggi.lee21@gmail.com>
2021-05-01 17:33:52 -0400

    finished most of JS functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const apiKey ='d8135c763a58289c458ea2f5c9b1d7a4';
 const TMDBMovieFun = {};
 
 const baseURL = 'https://api.themoviedb.org/3'
+const basePosterURL = 'https://image.tmdb.org/t/p/w500/';
 const movieRoute ='/search/movie';
 const peopleRoute = '/search/person';
 
@@ -13,8 +14,6 @@ let selectedURL = undefined;
 
 let userChoice = document.querySelector('#menuChoice').value;
 console.log(userChoice);
-
-let userSearch = document.querySelector('#userInput').value;
 
 if (userChoice === 'title'){
     selectedURL = movieURL;
@@ -28,13 +27,26 @@ else {
     selectedURL = undefined;
 }
 
+// const testURL = 'https://api.themoviedb.org/3/search/person?api_key=d8135c763a58289c458ea2f5c9b1d7a4&language=en-US&query=cruise&page=1&include_adult=false';
 
-TMDBMovieFun.getMovies = () => {
+
+//     const url = new URL(testURL);
+//     fetch(url).then((response) => {
+//         // console.log(response.json());
+//         return response.json();
+//     })
+//         .then((jsonResponse) => {
+//             console.log(jsonResponse);
+//     })
+
+
+
+TMDBMovieFun.getMovies = (userSearch) => {
 
     const url = new URL(selectedURL);
     url.search = new URLSearchParams({
         api_key: apiKey,
-        query: 'star'
+        query: userSearch
 
     })
 
@@ -43,9 +55,9 @@ TMDBMovieFun.getMovies = () => {
         return response.json();
     })
         .then((jsonResponse) => {
-
+            console.log('hi');
             TMDBMovieFun.displayPosters(jsonResponse.results);
-            console.log(jsonResponse);
+            console.log(jsonResponse.results);
             // for (let i=0; i<jsonResponse.results.length ;i++){
             //     console.log(jsonResponse.results[i]);
             // }
@@ -81,8 +93,6 @@ TMDBMovieFun.displayPosters = (simpleResultArray) => {
             const poster = document.createElement('img');
             poster.id = simpleResultArray[i].id;
 
-            const basePosterURL = "https://image.tmdb.org/t/p/w500/";
-
             poster.src = `${basePosterURL}${simpleResultArray[i].poster_path}`;
 
 
@@ -92,11 +102,7 @@ TMDBMovieFun.displayPosters = (simpleResultArray) => {
             // Document.getElementsByClassName('i');
             // console.log(simpleResultArray);
 
-            
-
-        }
-
-        document.querySelectorAll('img').addEventListener('click', function (event) {
+            poster.addEventListener('click', function (event) {
             console.log("try");
             // console.log(event);
             console.log(event.target);
@@ -105,52 +111,75 @@ TMDBMovieFun.displayPosters = (simpleResultArray) => {
             TMDBMovieFun.getMovieDetail(chosenMovieID);
         });
 
+        }
+
         
-
         // TMDBMovieFun.displayDetail(movie.id);
-
-
-
     // });
 }
-
-
 // TMDBMovieFun.addEventListener = 
-
-
-// TMDBMovieFun.displayDetail = (movieID) => {
-
+//TMDBMovieFun.displayDetail = (movieID) => {
 TMDBMovieFun.getMovieDetail = (chosenMovieID) => {
     const detailedMovieURL = `https://api.themoviedb.org/3/movie/${chosenMovieID}`;
 
     const url = new URL(detailedMovieURL);
     console.log(url);
     url.search = new URLSearchParams({
-        
-    
         api_key: apiKey
-
     })
 
     fetch(url).then((response) => {
-        // console.log(response.json());
         return response.json();
     })
         .then((jsonResponse) => {
-            TMDBMovieFun.displayPosters(jsonResponse.results);
+            
             console.log(jsonResponse);
 
-        })
+            TMDBMovieFun.displayMovieDetail(jsonResponse);
+
+        });
     
     
     }
-// }
+
+TMDBMovieFun.displayMovieDetail = (movieInfo) => {
+    
+    document.getElementById('simpleSearch').innerHTML ="";
+    
+    let title = movieInfo.original_title;
+    let overview = movieInfo.overview;
+    let posterURL = `${basePosterURL}${movieInfo.poster_path}`;
+    let release = movieInfo.release_date;
+    let runtime = movieInfo.runtime;
+    let vote = movieInfo.vote_average;
+    console.log(overview);
 
 
+
+    document.getElementById('simpleSearch').innerHTML =`
+            <img src="${posterURL}" alt="movie poster for ${title}" class = "detailMovie">
+            <h2>${title}</h2>
+            <p>${overview}</p>
+            <p>${release}</p>
+            <p>${runtime}</p>
+            <p>${vote}</p>`;
+}
+
+TMDBMovieFun.search = () => {
+
+
+    document.querySelector('form').addEventListener('submit', function(event){
+        event.preventDefault();
+        document.getElementById('simpleSearch').innerHTML =`<ul class="gallery"></ul>`;
+        let userSearch = document.querySelector('#userInput').value;
+        TMDBMovieFun.getMovies(userSearch);
+    })
+}
 
 
 TMDBMovieFun.init = () => {
-    TMDBMovieFun.getMovies();
+
+    TMDBMovieFun.search();
 }
 
 TMDBMovieFun.init();
